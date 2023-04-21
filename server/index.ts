4b0938a164b8ffdb933585e846a9cb3a0f532c27
Filip Chalupa <chalupa.filip@gmail.com>
2023-04-21 14:11:58 +0200

    Read port from env

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,12 @@
 import express from 'express'
 import { apiRouter } from './api/index'
 
-const port = false ? Number(0) : 8000
+const port = Number(process.env.PORT) || 8000
 
 const app = express()
 
-// app.use(apiRouter.routes())
-
 app.use(express.static('public'))
 app.use('/api', apiRouter)
-
-// // Serve public directory
-// app.use(async (context, next) => {
-// 	try {
-// 		await context.send({
-// 			root: `${Deno.cwd()}/public`,
-// 			index: 'index.html',
-// 		})
-// 	} catch {
-// 		next()
-// 	}
-// })
 
 app.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}.`)
